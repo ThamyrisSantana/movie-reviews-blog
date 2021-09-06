b0bb7acf448829206b5f159f62721ea7a1abd9c0
Thamyris <thamyrissatana3@gmail.com>
2021-09-06 12:44:18 -0300

    removing banner

--- components/banner/banner.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import Image from "next/image";
-// import bannerImg from "./background.jpg";
-import styles from "./banner.module.css";
-
-export default function Banner() {
-  return <div className={styles.bannerContainer}></div>;
-}
